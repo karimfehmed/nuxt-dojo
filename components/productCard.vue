63de269a56835bb3ceb976ae0c8291786fa421a4
karimfehmed <karimfehmed1998@gmail.com>
2023-06-21 13:36:24 +0500

    first commit

---
+++ components/productCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card text-center">
+        <div>
+        <img :src="product.image" alt="product thumb" class="thumb">
+
+        </div>
+        <p class="font-bold text-gray-500 truncate my-3">{{ product.title }}</p>
+        <NuxtLink :to="`/products/${product.id}`" class="btn my-3">View Details</NuxtLink>
+        
+    </div>
+</template>
+
+<script setup>
+const {product} = defineProps(['product'])
+</script>
+
+<style  scoped>
+.thumb{
+    max-height: 120px;
+    max-width: 70%;
+    margin: 0 auto;
+}
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="grid grid-cols-auto gap-2 justify-center sm:grid-cols-[repeat(auto-fit,minmax(300px,1fr))]">
+      <div v-for="p in products">
+        <productCard :product="p"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  definePageMeta({
+    layout: "products",
+  })
+
+  //  fetch the products
+  const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
+  useHead({
+    title: 'Nuxt Dojo | Merch',
+    meta: [
+      { name: 'description', content: 'Nuxt 3 Merch'}
+    ]
+  })
+</script>
